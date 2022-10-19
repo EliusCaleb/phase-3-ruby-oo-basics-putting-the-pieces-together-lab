319ff63205c7531cb594c1126f86b1e897afb331
EliusCaleb <eliuswanyoro@gmail.com>
2022-10-19 18:38:03 +0300

    Learning

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book
+    attr_reader :title
+    attr_writer :turn_page
+    attr_accessor :author, :page_count, :genre
+
+    def books=(properties)
+        author, page_count, turn_page,genre = properties.split
+        @author = author
+        @page_count = page_count
+        @genre  = genre
+    end
+    
+    def books
+        properties.strip
+    end
+    
+    
+
+    def initialize(title)
+        @title= title
+    end
+
+    def turn_page 
+         puts "Flipping the page...wow, you read fast!" 
+    end
+        
+
+end
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+
+class Shoe   
+    attr_reader :brand
+    attr_writer :cobble
+    attr_accessor :color, :size, :material, :condition
+
+    def shoes=(properties)
+        color, size, material,condition = properties.split
+        @color = color
+        @size = size
+        @material = material
+        @condition  = condition
+    end
+    
+    def shoes
+        properties.strip
+    end
+    
+    
+
+    def initialize(brand)
+        @brand= brand
+    end
+
+
+    def cobble
+       puts "Your shoe is as good as new!"
+       @condition= "new"
+    end
+
+end
+shoe = Shoe.new("Adidas")
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
